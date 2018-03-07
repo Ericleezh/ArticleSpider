957bf2bc5c6a1c7c152f2953fe397f3ca4a26063
Jonnyleezh <1134865224@qq.com>
2018-03-08 00:49:54 +0800

    添加了对问题以及问题回复的爬取，未入库(mysql)

--- ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/spiders/zhihu.py
@@ -7,7 +7,7 @@ from hashlib import sha1
 from scrapy.loader import ItemLoader
 from ArticleSpider.items import ZhihuQuestionItem,ZhihuAnswerItem
 
-#兼容py2和py3
+#兼容py2和py3.
 try:
     import  urlpare as parse
 except:
@@ -17,9 +17,8 @@ class ZhihuSpider(scrapy.Spider):
     name = 'zhihu'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['http://www.zhihu.com/']
-    #question的第一页answer的请求url
+    #question第一页answer的请求url
     start_answer_url = "https://www.zhihu.com/api/v4/questions/{0}/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&limit={1}&offset={2}&sort_by=default"
-
     phone = '+8615112580677'  # 手机号
     password = 'lizhenhao0524'  # 密码
     client_id = 'c3cef7c66a1843f8b3a9e6a1e3160e20'
