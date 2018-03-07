c0117dde52d1c1272c0a7e01f1364ca88a33b4e6
Jonnyleezh <1134865224@qq.com>
2018-03-08 00:32:11 +0800

    添加了对问题以及问题回复的爬取，未入库(mysql)

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -74,3 +74,29 @@ class JobBoleArticleItem(scrapy.Item) :
         output_processor=Join(",")
     )
     content = scrapy.Field()
+
+class ZhihuQuestionItem(scrapy.Item):
+    #知乎问题的Item
+    zhihu_id = scrapy.Field()
+    topics = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    content = scrapy.Field()
+    answer_num = scrapy.Field()
+    comments_num = scrapy.Field()
+    watch_user_num = scrapy.Field()
+    click_num = scrapy.Field()
+    crawl_time = scrapy.Field()
+
+class ZhihuAnswerItem(scrapy.Item):
+    #知乎的问题回答Item
+    zhihu_id = scrapy.Field()
+    url = scrapy.Field()
+    question_id = scrapy.Field()
+    author_id = scrapy.Field()
+    content = scrapy.Field()
+    praise_num = scrapy.Field()
+    comments_num = scrapy.Field()
+    create_time = scrapy.Field()
+    update_time = scrapy.Field()
+    crawl_time = scrapy.Field()
--- ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/spiders/zhihu.py
@@ -2,7 +2,11 @@
 import scrapy,time,hmac,json,requests
 import execjs
 import re
+import datetime
 from hashlib import sha1
+from scrapy.loader import ItemLoader
+from ArticleSpider.items import ZhihuQuestionItem,ZhihuAnswerItem
+
 #兼容py2和py3
 try:
     import  urlpare as parse
@@ -13,7 +17,8 @@ class ZhihuSpider(scrapy.Spider):
     name = 'zhihu'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['http://www.zhihu.com/']
-    sessiona = requests.Session()
+    #question的第一页answer的请求url
+    start_answer_url = "https://www.zhihu.com/api/v4/questions/{0}/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&limit={1}&offset={2}&sort_by=default"
 
     phone = '+8615112580677'  # 手机号
     password = 'lizhenhao0524'  # 密码
@@ -37,14 +42,62 @@ class ZhihuSpider(scrapy.Spider):
             print(url)
             match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$)",url)
             if match_obj:
+                #如果提取到question的页面，则交给提取函数进行处理
                 request_url = match_obj.group(1)
-                question_url = match_obj.group(2)
-
+                #深度优先遍历的入口
                 yield scrapy.Request(request_url,headers=self.headers,callback=self.parse_question)
+            else:
+                #若不是则进一步跟踪
+                yield scrapy.Request(url,headers=self.headers,callback=self.parse)
 
     def parse_question(self,response):
         #处理question，从页面中提取出具体的question item
-        pass
+        match_obj  = re.match("(.*zhihu.com/question/(\d+))(/|$)",response.url)
+        if match_obj:
+            question_id = int(match_obj.group(2))
+
+        item_loader = ItemLoader(item=ZhihuQuestionItem(),response=response)
+        item_loader.add_css("title",".QuestionHeader .QuestionHeader-content .QuestionHeader-main h1.QuestionHeader-title::text")
+        item_loader.add_css("content","div.QuestionHeader-detail")
+        item_loader.add_value("url",response.url)
+        item_loader.add_value("zhihu_id",question_id)
+        item_loader.add_css("answer_num","h4.List-headerText span::text")
+        item_loader.add_css("comments_num",".QuestionHeader-Comment button::text")
+        item_loader.add_css("watch_user_num",".NumberBoard-itemValue::text")
+        item_loader.add_css("topics",".QuestionHeader-topics .Popover div::text")
+
+        question_item = item_loader.load_item()
+
+        # 路由到下载器
+        yield scrapy.Request(self.start_answer_url.format(question_id,5,0),headers=self.headers,callback=self.parse_answer)
+        yield question_item #路由到pipeline中
+
+    def parse_answer(self,response):
+        #处理question的answer
+        ans_json = json.loads(response.text)
+        is_end = ans_json["paging"]["is_end"]
+        next_url = ans_json["paging"]["next"]
+
+        #提取answer的具体字段
+        for answer in ans_json["data"]:
+            answer_item = ZhihuAnswerItem()
+            answer_item["zhihu_id"] = answer["id"]
+            answer_item["url"] = answer["url"]
+            answer_item["question_id"] = answer["question"]["id"]
+            answer_item["author_id"] = answer["author"]["id"] if "id" in answer["author"] else None
+            answer_item["content"] = answer["content"] if "content" in answer else None
+            answer_item["praise_num"] = answer["voteup_count"]
+            answer_item["comments_num"] = answer["comment_count"]
+            answer_item["create_time"] = answer["created_time"]
+            answer_item["update_time"] = answer["updated_time"]
+            answer_item["crawl_time"] = datetime.datetime.now()
+
+            yield answer_item
+        if not is_end:
+            yield  scrapy.Request(next_url,headers=self.headers,callback=self.parse_answer)
+
+
+
 
     # def get_captcha(data,need_cap):
     #     # 处理验证码
